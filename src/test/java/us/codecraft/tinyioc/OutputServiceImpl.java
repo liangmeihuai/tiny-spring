df67f1315608aac30e1ea9e6a06783f47d94f41e
meihuai.liang <meihuai.liang@tendcloud.com>
2017-06-22 18:20:37 +0800

    channge

--- src/test/java/us/codecraft/tinyioc/OutputServiceImpl.java
+++ src/test/java/us/codecraft/tinyioc/OutputServiceImpl.java
@@ -4,6 +4,15 @@ package us.codecraft.tinyioc;
  * @author yihua.huang@dianping.com
  */
 public class OutputServiceImpl implements OutputService {
+    private HelloWorldService helloWorldService;
+
+    public HelloWorldService getHelloWorldService() {
+        return helloWorldService;
+    }
+
+    public void setHelloWorldService(HelloWorldService helloWorldService) {
+        this.helloWorldService = helloWorldService;
+    }
 
     @Override
     public void output(String text){
